2ab2ff22a855ea8c170387837a2b547b710f0814
Gaya <gaya@theclevernode.com>
2021-04-30 11:13:37 +0200

    example project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [config, setConfig] = useState(null);
+
+  useEffect(() => {
+    fetch('/config.json')
+      .then((response) => response.json())
+      .then((jsonConfig) => setConfig(jsonConfig));
+  }, []);
+
+  if (config === null) {
+    return (
+      <div className="App">
+        Loading
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {config.url}
     </div>
   );
 }
